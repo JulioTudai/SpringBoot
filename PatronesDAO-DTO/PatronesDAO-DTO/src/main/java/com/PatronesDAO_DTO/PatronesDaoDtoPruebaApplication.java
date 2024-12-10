a138bd9dc3a5c90afc7cc4ad6b2a84d9702c7e41
JulioTudai <Juliotwete@hotmail.com>
2024-12-10 19:46:00 -0300

    primer commit empezando dao dto

---
+++ PatronesDAO-DTO/PatronesDAO-DTO/src/main/java/com/PatronesDAO_DTO/PatronesDaoDtoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.PatronesDAO_DTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesDaoDtoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesDaoDtoPruebaApplication.class, args);
+	}
+
+}
---
+++ PatronesDAO-DTO/PatronesDAO-DTO/src/test/java/com/PatronesDAO_DTO/PatronesDaoDtoPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PatronesDAO_DTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesDaoDtoPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
